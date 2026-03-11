4179d537475617e82cf29bc41e9c0db6cabc9784
delef <idelef@gmail.com>
2026-03-11 23:42:45 +0400

    fix: changed storage format

--- storage.py
+++ storage.py
@@ -2,21 +2,20 @@ import pickle
 
 from models import AddressBook, NoteBook
 
+FILENAME = "assistant_data.pkl"
 
-# Save all data (address book + notebook) to a file using pickle
-def save_data(book, notebook, filename="assistant_data.pkl"):
-    with open(filename, "wb") as f:
+
+# Save all data to a file using pickle
+def save_data(book, notebook):
+    with open(FILENAME, "wb") as f:
         pickle.dump({"book": book, "notebook": notebook}, f)
 
 
 # Load all data from file, or return new instances if file doesn't exist
-def load_data(filename="assistant_data.pkl"):
+def load_data():
     try:
-        with open(filename, "rb") as f:
+        with open(FILENAME, "rb") as f:
             data = pickle.load(f)
-            # Support old format where only book was saved
-            if isinstance(data, AddressBook):
-                return data, NoteBook()
             return data["book"], data["notebook"]
     except FileNotFoundError:
         return AddressBook(), NoteBook()
