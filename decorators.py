b00b6887fec87ae5a5fa5b52334fa6594358e419
delef <idelef@gmail.com>
2026-03-14 21:47:15 +0400

    fix: #12, #13, #14

--- decorators.py
+++ decorators.py
@@ -1,13 +1,16 @@
 # Decorator that catches common input errors and returns user-friendly messages
-def input_error(func):
-    def inner(*args, **kwargs):
-        try:
-            return func(*args, **kwargs)
-        except KeyError:
-            return "Contact not found."
-        except ValueError as e:
-            return str(e)
-        except IndexError:
-            return "Not enough arguments provided."
+def input_error(entity="Contact"):
+    def decorator(func):
+        def inner(*args, **kwargs):
+            try:
+                return func(*args, **kwargs)
+            except KeyError:
+                return f"{entity} not found."
+            except ValueError as e:
+                return str(e)
+            except IndexError:
+                return "Not enough arguments provided."
 
-    return inner
+        return inner
+
+    return decorator
